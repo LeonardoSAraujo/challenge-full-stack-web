0cc3fbcf269dfbf8979545b98000797163a1562a
LeonardoSAraujo <N.ardo@hotmail.com>
2022-09-28 14:53:26 -0300

    function exports complement

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,104 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { PrismaClient,Prisma } = require('@prisma/client')
-
+const {findStudents,updateStudent,createStudent} = require('../functions/students/studentsFunctions')
 
 const prisma = new PrismaClient()
 /* GET for list all users */
-
-
-const createStudent = async(student)=> {
-  try {
-     await prisma.Students.create({
-      data:{
-        name: student.name,
-        email: student.email,
-        cpf: student.cpf,
-        register: student.register
-      },
-    })
-    return('Cadastro efetuado')
-  }  catch (e) {
-    let creationerror = 'Erro ao cadastrar, tente novamente'
-    if (e instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log(
-        `There is a unique constraint violation, a new user cannot be created with ${e.meta.target}`
-      )
-      
-      if (e.code === 'P2002') {
-        if((e.meta.target == 'Students_cpf_key') || (e.meta.target == 'Students_register_key')){
-          creationerror = 'Estudante já cadastrado'
-        }else if(e.meta.target == 'Students_email_key'){
-          creationerror = 'Email já está em uso'
-        }
-      }
-    }
-    return(creationerror)
-  }
-}
-
-const findStudents = async(value)=> {
-  const userslist = await prisma.Students.findMany({
-    where:{
-      OR: [
-        {
-          name: {
-            contains: value
-          }
-        },
-        {
-          email: {
-            contains: value
-          }
-        },
-        {
-          cpf: {
-            contains: value
-          }
-        },
-        {
-          register: {
-            equals: isNaN(parseInt(value)) === true?undefined:parseInt(value)
-          }
-        }
-        
-      ]
-      
-    }
-  })
-  return userslist
-}
-
-const updateStudent = async(id,student)=> {
-  try {
-     await prisma.Students.update({
-      where:{
-        id: parseInt(id)
-      },
-      data:{
-        name: student.name,
-        email: student.email,
-      },
-    })
-    return('Cadastro alterado com sucesso')
-  }  catch (e) {
-    console.log(e)
-    let updateerror = 'Erro ao cadastrar, tente novamente'
-    if (e instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log(
-        `There is a unique constraint violation, ${e.meta.cause}`
-      )
-      
-      if (e.code === 'P2025') {
-        updateerror = 'Usuário não encontrado'
-      }
-    }
-    return(updateerror)
-  }
-}
-
-
-
 router.get('/',async function(req, res, next) {
   const datausers = await findStudents(req.query.search)
   res.json({
